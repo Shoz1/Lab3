b176d7ce2b1b4953bce96ff25de396be80861da8
Женя Босюк <bosuk21@gmail.com>
2019-04-14 13:13:06 +0300

    v1

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp5
+namespace CSharp3
 {
+
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
+
+            Console.WriteLine("Введите ax,bx,cx");
+
+            for (int i = 0; i < 3; i++)
+            {
+                vector.X[i] = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите ay,by,cy");
+
+            for (int i = 0; i < 3; i++)
+            {
+            vector.Y[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите az,bz,cz");
+
+            for (int i = 0; i < 3; i++)
+             {
+               vector.Z[i] = int.Parse(Console.ReadLine());
+             }
+
+            Console.Write("Векторы a,b,c : ");
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write($"{vector.X[i]},{vector.Y[i]},{vector.Z[i]}");
+            }
+
+            Console.WriteLine(vector.GetL());
+            /*
+            Console.WriteLine("длинна ветора a: " + vector.GetLa()) ;
+            */     
         }
     }
+
 }
---
+++ ConsoleApp5/vector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp3
+{
+    
+    public class vector
+    {
+        private static double[] x = new double[3];
+        private static double[] y = new double[3];
+        private static double[] z = new double[3];
+
+        public static double[] X { get => x; set => x = value; }
+        public static double[] Y { get => y; set => y = value; }
+        public static double[] Z { get => z; set => z = value; }
+        public static double la,lb,lc;
+        public static double result;
+
+
+
+
+        //public static double operator +( A, B)
+        //  {
+        //    return A + B;
+        // }
+
+        public static double GetL()
+        {
+
+            la = Math.Sqrt(Math.Pow(X[0], 2) + Math.Pow(Y[0], 2) + Math.Pow(Z[0], 2));
+            lb = Math.Sqrt(Math.Pow(X[1], 2) + Math.Pow(Y[1], 2) + Math.Pow(Z[1], 2));
+            lc = Math.Sqrt(Math.Pow(X[2], 2) + Math.Pow(Y[2], 2) + Math.Pow(Z[2], 2));
+
+            return result; 
+        }
+    }   
+    
+}
+
